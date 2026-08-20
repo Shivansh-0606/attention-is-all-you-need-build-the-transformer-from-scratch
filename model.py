9723719fb729bac5f623a43a88be362ee2b116a0
Shivansh Jhalani <shivanshjhalani123@gmail.com>
2026-08-20 19:10:25 +0530

    Step 20: softmax_attention_weights

--- model.py
+++ model.py
@@ -225,8 +225,14 @@ def mask_attention_scores_with_neg_inf(scores, mask):
 
     return torch.where(mask , scores , float('-inf'))
 
-# Step 20 - softmax_attention_weights (not yet solved)
-# TODO: implement
+# Step 20 - softmax_attention_weights
+import torch
+
+def softmax_attention_weights(masked_scores):
+    # TODO: softmax over the last axis, zeroing rows that are entirely -inf
+    m = torch.nn.Softmax(dim = -1)
+
+    return torch.where(masked_scores!=float('-inf'),m(masked_scores),0)
 
 # Step 21 - apply_attention_weights_to_values (not yet solved)
 # TODO: implement

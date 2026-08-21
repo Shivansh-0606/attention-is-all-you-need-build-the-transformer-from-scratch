c0e2e68c64c6f6e47716c895acef34aa447c8afd
Shivansh Jhalani <shivanshjhalani123@gmail.com>
2026-08-21 19:31:00 +0530

    Step 26: apply_linear_projection

--- model.py
+++ model.py
@@ -290,8 +290,14 @@ def merge_heads_back_to_model_dim(multi_head_tensor):
     
     return multi_head_tensor.reshape(B,L,H*d_k)
 
-# Step 26 - apply_linear_projection (not yet solved)
-# TODO: implement
+# Step 26 - apply_linear_projection
+def apply_linear_projection(x, weight, bias):
+    # TODO: return x @ weight^T + bias (bias may be None) with shape (..., out_features)
+    out = x @ weight.T
+
+    if bias is not None:
+        out+=bias
+    return out
 
 # Step 27 - project_to_query_key_value (not yet solved)
 # TODO: implement
